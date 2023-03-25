80d7635f7ff817ae035c5dc895eaf250da9a463d
yxj777 <18932839185@163.com>
2023-03-26 06:54:39 +0800

    test1

--- 鸟哥的Linux私房菜（基础篇）/5.文件与文件系统的压缩、打包与备份/5.2打包指令：tar.mk
+++ 鸟哥的Linux私房菜（基础篇）/5.文件与文件系统的压缩、打包与备份/5.2打包指令：tar.mk
@@ -19,3 +19,81 @@
 -P（大写） ：保留绝对路径，亦即允许备份数据中含有根目录存在之意；
 --exclude=FILE：在压缩的过程中，不要将 FILE 打包！
 ```
+- ### 常用方式
+  - 压　缩：tar -jcv -f filename.tar.bz2 要被压缩的档案或目录名称
+  - 查　询：tar -jtv -f filename.tar.bz2
+  - 解压缩：tar -jxv -f filename.tar.bz2 -C 欲解压缩的目录
+- ### 使用 tar 加入 -z， -j 或 -J 的参数备份目录
+      ```
+      root@yxj-computer:~# time tar -Jpcv -f /root/etc.tar.xz /etc
+      ....（前面省略）....
+      real	0m1.511s          #time可以显示运行时间，为1.511s
+      user	0m1.482s
+      sys	0m0.080s
+
+      root@yxj-computer:~# ll /root/etc*
+      -rw-r--r-- 1 root root 1036384  3月 26 10:08 /root/etc.tar.xz
+      ```
+- ### 查看tar文件，与备份文件名有否根目录的意义
+      ```
+      root@yxj-computer:~# tar -Jtv -f /root/etc.tar.xz
+      ....（前面省略）....
+      drwxr-xr-x root/root         0 2023-03-25 15:14 etc/
+      drwxr-xr-x root/root         0 2023-03-11 08:29 etc/pam.d/
+      -rw-r--r-- root/root       168 2022-02-23 23:12 etc/pam.d/ppp
+      -rw-r--r-- root/root       330 2022-02-08 16:47 etc/pam.d/sudo
+      -rw-r--r-- root/root       581 2021-11-11 23:42 etc/pam.d/chsh
+      ```
+  - 在 tar 所记录的文件名就是解压缩后的实际文件名
+  - 如果有根目录（-P参数），解压后的文件名就会是绝对路径
+    - 源数据就会被备份数据所覆盖
+- ### 备份数据解压，并解压到特定目录（-C）
+      ```
+      root@yxj-computer:~# tar -Jxv -f /root/etc.tar.xz           <==解压到本目录
+      ....（省略-v显示）....
+      root@yxj-computer:~# tar -Jxv -f /root/etc.tar.xz -C /tmp   <==解压到特定目录
+      ....（省略-v显示）....
+      ```
+
+- ### 仅解开单一文件
+  - 使用 -jtv 找到文件名，然后将该文件名解开即可
+      ```
+      root@yxj-computer:~# tar -Jtv -f /root/etc.tar.xz | grep 'shadow'
+      -rw-r----- root/shadow     999 2023-03-18 10:05 etc/gshadow-
+      -rw-r----- root/shadow    1575 2023-03-11 14:31 etc/shadow-
+      -rw-r----- root/shadow    1676 2023-03-15 08:52 etc/shadow
+      -rw-r----- root/shadow     970 2023-03-18 10:05 etc/gshadow
+
+      # 解开单一文件语法
+      # [root@www ~]# tar -Jxv -f 打包档.tar.xz 待解开档名
+      root@yxj-computer:~# tar -Jxv -f /root/etc.tar.xz etc/shadow
+      etc/shadow
+
+      root@yxj-computer:~# ll etc/shadow
+      -rw-r----- 1 root shadow 1676  3月 15 08:52 etc/shadow
+      ```
+- ### 打包某目录，但不含该目录下的某些文件
+  - 使用 --exclude=FILE
+      ```
+      # 打包/etc，/root,但不打包/root/etc*和/root/system.tar.xz（自己）
+      [root@www ~]# tar -jcv  -f /root/system.tar.bz2 --exclude=/root/etc* \
+      > --exclude=/root/system.tar.bz2  /etc /root
+      ```
+  - 可以使用反斜线（\）+ [enter]，进行换行输入
+- ### 备份比某个时刻还要新的文件
+  - 使用“ --newer ” 和“ --newer-mtime ”
+    - --newer ：后续的日期包含“ mtime 与 ctime ”
+    - --newer-mtime ： 仅是 mtime
+      ```
+      # 比2023/03/25新的文件
+      tar -jcv -f /root/etc.newer.then.passwd.tar.bz2 \
+      --newer-mtime="2023/03/25" /etc/*
+      ```
+- ### tarfile，tarball
+  - tarfile ：仅是打包”，就是“ tar -cv -f file.tar “
+  - tarball ：还进行压缩，就是“ tar -jcv -f file.tar.bz2 ”
+- ### 特殊应用：利用管线命令与数据流
+```
+# 将 /etc 整个目录一边打包一边在 /tmp 解开
+[root@www ~]# tar -cvf - /etc | tar -xvf -
+```
